8cb292df33acd7eaf27f115d31b18d95cc28f5d6
Andre Hehn <a.hehn@web.de>
2022-05-31 20:01:36 +0200

    loads all cards in div

--- script.js
+++ script.js
@@ -1,40 +1,67 @@
 
 
+
 async function loadPokemonUrls() {
     let urlPokemonList = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
     let response = await fetch(urlPokemonList);
     let responseAsJson = await response.json();
-    for (let i = 0; i <responseAsJson.length; i++) {
-    let allPokemonArray = responseAsJson['results'][i]['url'];
-        let urlPokemon = allPokemonArray;
+    await getPokemonURL(responseAsJson);
+}
+
+
+async function getPokemonURL(responseAsJson) {
+    for (let i = 0; i < responseAsJson['results'].length; i++) {
+        let urlPokemon = await responseAsJson['results'][i]['url'];
         let responsePokemon = await fetch(urlPokemon);
         let responsePokemonAsJson = await responsePokemon.json();
-        let name = responsePokemonAsJson['name'];
-        let urlArtwork = responsePokemonAsJson['sprites']['official-artwork']['front_default'];
-        let types
-        let baseStats
-        let StatName
-        let abilityName
-        let weight = responsePokemonAsJson['weight'];
+        PrepForRenderCard(responsePokemonAsJson);
+    }
+}
 
+
+async function PrepForRenderCard(responsePokemonAsJson) {
+    let name = await responsePokemonAsJson['name'];
+    let urlArtwork = await responsePokemonAsJson['sprites']['other']['official-artwork']['front_default'];
+    let weight = await responsePokemonAsJson['weight'];
+    let types = [];
+    let stats = {
+        'name': [],
+        'baseStat': []
+    };
+    let abilityName = [];
+    await fillVar(responsePokemonAsJson, types, abilityName, stats);
+    renderCard(name, urlArtwork, weight, types, stats, abilityName);
+}
+
+
+async function fillVar(responsePokemonAsJson, types, abilityName, stats) {
+    for (let i = 0; i < responsePokemonAsJson['types'].length; i++) {
+        types.push(await responsePokemonAsJson['types'][i]['type']['name']);
+    }
+    for (let i = 0; i < responsePokemonAsJson['stats'].length; i++) {
+        stats['name'].push(await responsePokemonAsJson['stats'][i]['name']);
+        stats['baseStat'].push(await responsePokemonAsJson['stats'][i]['base_stat']);
+    }
+    for (let i = 0; i < responsePokemonAsJson['abilities'].length; i++) {
+        abilityName.push(await responsePokemonAsJson['abilities'][i]['ability']['name']);
 
     }
 }
 
 
+function renderCard(name, urlArtwork, weight, types, stats, abilityName) {
+    renderCardSmall(name, urlArtwork, types);
+    renderCardBig(name, urlArtwork, weight, types, stats, abilityName);
+}
 
 
-
-/* 
-
-
-types ->iterate with length ->type > name
-
-
-stats -> iterate with length -> base_stat
-                             -> stat -> name
-
-abilities -> iterate with length -> ability -> name                             
-
-
-*/
+function renderCardSmall(name, urlArtwork, types) {
+    document.getElementById('cards').innerHTML += `
+<div class="small-Card ${types[0]} ${types[1]}" onclick="modal(${name})">
+<h2>${name}</h2>
+<img class= "smallPic" src ="${urlArtwork}">
+<span>${types[0]} ${types[1]}</span>
+</div>
+`;
+}
+function renderCardBig(name, urlArtwork, weight, types, stats, abilityName){}
