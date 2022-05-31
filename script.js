a0bb9d066e72b1e10de7bf86371eb541e297d889
Andre Hehn <a.hehn@web.de>
2022-05-31 22:22:43 +0200

    css optimized

--- script.js
+++ script.js
@@ -19,31 +19,31 @@ async function getPokemonURL(responseAsJson) {
 }
 
 
-async function PrepForRenderCard(responsePokemonAsJson) {
-    let name = await responsePokemonAsJson['name'];
-    let urlArtwork = await responsePokemonAsJson['sprites']['other']['official-artwork']['front_default'];
-    let weight = await responsePokemonAsJson['weight'];
+function PrepForRenderCard(responsePokemonAsJson) {
+    let name = responsePokemonAsJson['name'];
+    let urlArtwork = responsePokemonAsJson['sprites']['other']['official-artwork']['front_default'];
+    let weight = responsePokemonAsJson['weight'];
     let types = [];
     let stats = {
         'name': [],
         'baseStat': []
     };
     let abilityName = [];
-    await fillVar(responsePokemonAsJson, types, abilityName, stats);
+    fillVar(responsePokemonAsJson, types, abilityName, stats);
     renderCard(name, urlArtwork, weight, types, stats, abilityName);
 }
 
 
-async function fillVar(responsePokemonAsJson, types, abilityName, stats) {
+function fillVar(responsePokemonAsJson, types, abilityName, stats) {
     for (let i = 0; i < responsePokemonAsJson['types'].length; i++) {
-        types.push(await responsePokemonAsJson['types'][i]['type']['name']);
+        types.push(responsePokemonAsJson['types'][i]['type']['name']);
     }
     for (let i = 0; i < responsePokemonAsJson['stats'].length; i++) {
-        stats['name'].push(await responsePokemonAsJson['stats'][i]['name']);
-        stats['baseStat'].push(await responsePokemonAsJson['stats'][i]['base_stat']);
+        stats['name'].push(responsePokemonAsJson['stats'][i]['name']);
+        stats['baseStat'].push(responsePokemonAsJson['stats'][i]['base_stat']);
     }
     for (let i = 0; i < responsePokemonAsJson['abilities'].length; i++) {
-        abilityName.push(await responsePokemonAsJson['abilities'][i]['ability']['name']);
+        abilityName.push(responsePokemonAsJson['abilities'][i]['ability']['name']);
 
     }
 }
@@ -63,10 +63,10 @@ function renderCardSmall(name, urlArtwork, types) {
 <div id ="type${name}" class="types"></div>
 </div>
 `;
-for (let i = 0; i < types.length; i++) {
-   document.getElementById('type'+ name).innerHTML+=`<div class= ${types[i]}>${types[i]}</div>`;  
- }
+    for (let i = 0; i < types.length; i++) {
+        document.getElementById('type' + name).innerHTML += `<div class= "type ${types[i]}">${types[i]}</div>`;
+    }
 }
 
 
-function renderCardBig(name, urlArtwork, weight, types, stats, abilityName){}
+function renderCardBig(name, urlArtwork, weight, types, stats, abilityName) { }
